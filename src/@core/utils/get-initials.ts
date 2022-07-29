ba7cdddba87cff0af135a3a0e1431c652ba0ca8e
Quang Blue <quangblue1603@gmail.com>
2022-07-30 02:56:23 +0700

    fix : avarta string

--- src/@core/utils/get-initials.ts
+++ src/@core/utils/get-initials.ts
@@ -1,3 +1,6 @@
 // ** Returns initials from string
 export const getInitials = (string: string) =>
-  string.split(/\s/).reduce((response, word) => (response += word.slice(0, 1)), '')
+  string
+    .split(/\s/)
+    .reduce((response, word) => (response += word.slice(0, 1)), '')
+    .substring(0, 2)
